b9319a8d7fcd5b525011e5cbf75c45ddb901e2e7
sakura <sakura@163.com>
2021-05-26 16:41:26 +0800

    初始化

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-switch v-model="value" active-color="#13ce66" inactive-color="#ff4949">
+  </el-switch>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+       value:true
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
