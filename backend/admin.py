02f09a17bf32493b6b541437843a27f1733674b8
sakura <sakura@163.com>
2021-05-31 21:12:10 +0800

    添加快递信息类

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,11 @@
+from backend.models import ExpressInfo
 from django.contrib import admin
 
 # Register your models here.
+
+
+class ExpressList(admin.ModelAdmin):
+    list_display = ['express_id']
+
+
+admin.site.register(ExpressInfo, ExpressList)
--- backend/models.py
+++ backend/models.py
@@ -2,20 +2,22 @@ from django.db import models
 
 # Create your models here.
 
-# 暨大总仓库
+# 快递信息
 
 
-class MianWarehouse(models.Model):
+class ExpressInfo(models.Model):
+    # id
     express_id = models.PositiveIntegerField(primary_key=True)
+    # 建筑号数
     building = models.CharField(max_length=50)
+    # 收件人
     receiver = models.CharField(max_length=10)
+    # 手机
     phone = models.CharField(max_length=12)
+    # 备注
+    note = models.TextField()
+    # 位置号 （A5-2-2210）
+    locate = models.CharField(max_length=15)
 
     def __str__(self):
         return self.express_id
-
-# 楼宇分仓
-
-
-class MinorWarehouse(models.Model):
-    express_id = models.PositiveIntegerField(primary_key=True)
